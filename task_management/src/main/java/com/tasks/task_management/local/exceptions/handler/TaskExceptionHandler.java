9e35fe316b5073703b0200d81bb09cdaca756796
Bavlly magid <pavllymagid04@gmail.com>
2024-09-12 19:25:36 +0300

    [Remove] Default security

--- task_management/src/main/java/com/tasks/task_management/local/exceptions/handler/TaskExceptionHandler.java
+++ task_management/src/main/java/com/tasks/task_management/local/exceptions/handler/TaskExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.tasks.task_management.local.exceptions.handler;
 
 import com.tasks.task_management.local.exceptions.body.Commence;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,7 +11,7 @@ public class TaskExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<Commence> handleException(Exception e) {
-        Commence commence = new Commence(e.getMessage(), 400, System.currentTimeMillis());
+        Commence commence = new Commence(e.getMessage(), HttpStatus.BAD_REQUEST.value(), System.currentTimeMillis());
         return ResponseEntity.badRequest().body(commence);
     }
 }
